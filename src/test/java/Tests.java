4451fb0766eeb08238d44aee45983024fbef187e
Vitaliy Ismagilov <ismagilov.vel@gmail.com>
2019-02-28 13:11:36 +0200

    Add some more configurations

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,6 +1,9 @@
+import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.Description;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -11,17 +14,32 @@ import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.WebDriverRunner.addListener;
 
-public class Tests extends TestBase {
+public class Tests {
 
     @BeforeAll
     public static void setUp() {
-        addListener(new Highlighter());
+        SelenideLogger.addListener("AllureSelenide",
+                new AllureSelenide().screenshots(true).savePageSource(true));
+
+        System.setProperty("java.util.logging.SimpleFormatter.format",
+                "%1$tT %4$s %5$s%6$s%n"); // for pretty logging in console
+
+        addListener(new Highlighter()); // UI highlighting while running tests
     }
+
+    @AfterAll
+    public static void tearDown() {
+        SelenideLogger.removeListener("AllureSelenide");
+        //WebDriverRunner.clearBrowserCache();
+        //clearBrowserCookies();
+        //close();
+    }
+
 
     @Test
     @Tag("fast")
     @Severity(SeverityLevel.NORMAL)
-    @Description ("Allure description")
+    @Description("Allure description")
     public void first_test() {
         open("/");
         Menu goTo = page(Menu.class);
